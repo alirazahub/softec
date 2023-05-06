f72b65fef2013e08b47e7084ecefcba81bb74a33
alirazahb <aliraza.ar7785@gmail.com>
2023-05-06 15:30:34 +0500

    getting all products with user's wishlist

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js';
-import {verifyCustomer }from '../middleware/verifyCustomer.js';
+import { verifyCustomer } from '../middleware/verifyCustomer.js';
+import Customer from '../models/customerModel.js';
 
 const router = express.Router();
 
@@ -29,21 +30,6 @@ router.post('/addProduct', asyncHandler(async (req, res) => {
     }
 }))
 
-// getting all products with reviews and ratings
-router.get('/getProducts', asyncHandler(async (req, res) => {
-    try {
-        const products = await Product.find({}).populate('reviews.user', 'name')
-        if (products) {
-            res.status(201).json(products)
-        } else {
-            res.status(400).json({ message: "Invalid product data" })
-        }
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}))
-
-
 // adding revwing and rating to a product
 router.post('/addReview/:prodId', verifyCustomer, asyncHandler(async (req, res) => {
     const { rating, review } = req.body;
@@ -51,13 +37,12 @@ router.post('/addReview/:prodId', verifyCustomer, asyncHandler(async (req, res) 
     try {
         const product = await Product.findOne({ _id: prodId })
         if (product) {
-            console.log(req.user._id)
-            const alreadyReviewed = product.reviews.find(r => r.user.toString() === req.user._id.toString());
+            const alreadyReviewed = product.reviews.find(r => r.user._id.toString() === req.user.id.toString());
             if (alreadyReviewed) {
                 res.status(400).json({ message: "You already reviewed the product" })
             } else {
                 const revieww = {
-                    user: req.user._id,
+                    user: req.user.id,
                     review: review,
                     rating: Number(rating)
                 }
@@ -72,5 +57,39 @@ router.post('/addReview/:prodId', verifyCustomer, asyncHandler(async (req, res) 
         res.status(400).json({ message: error.message })
     }
 }))
+
+// getting all products with ratings, reviews ,and is in the wishlist of current user or not
+router.get('/getAllProducts', verifyCustomer, asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find({})
+        if (products) {
+            const user = await Customer.findOne({ _id: req.user.id })
+            const productsWithRating = products.map(product => {
+                const isWishlisted = user.wishlist.includes(req.user.id)
+                return {
+                    _id: product._id,
+                    title: product.title,
+                    stock: product.stock,
+                    description: product.description,
+                    marketPrice: product.marketPrice,
+                    sellingPrice: product.sellingPrice,
+                    image: product.image,
+                    inventory: product.inventory,
+                    minimumAge: product.minimumAge,
+                    rating: product.rating,
+                    numReviews: product.numReviews,
+                    reviews: product.reviews,
+                    isWishlisted: isWishlisted
+                }
+            })
+            res.status(200).json(productsWithRating)
+        } else {
+            res.status(400).json({ message: "No products found" })
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}))
+
 
 export default router;
