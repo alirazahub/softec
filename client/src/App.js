679b9745dad80f2a6b45a8f1b61945b6fd43a74a
Abdul Hadi <abdulhadi.reality@gmail.com>
2023-05-06 13:34:20 +0500

    item card component added in app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { Card } from 'antd';
-const { Meta } = Card;
+import ItemCard from './components/ItemCard'
 
 const App = () => {
   return (
     <div>
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      <ItemCard />
     </div>
   )
 }
 
 export default App
-
-
-
-// comment 
